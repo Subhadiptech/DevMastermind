450e9d76e3b39f28f152a4b9be14596b672aeeb3
Subhadiptech <subhadipdhn@gmail.com>
2023-11-06 17:48:48 +0530

    hilt initialized

---
+++ app/src/main/java/com/ersubhadip/devmastermind_gamifyyourdevelopmentjourney/DevMastermindApplication.kt
@@ -0,0 +1,7 @@
+package com.ersubhadip.devmastermind_gamifyyourdevelopmentjourney
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DevMastermindApplication : Application()
