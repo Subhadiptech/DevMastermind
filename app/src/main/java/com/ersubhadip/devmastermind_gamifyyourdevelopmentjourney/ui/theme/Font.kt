aeb74367e3f71406308fbf3ceaefcec91973ad82
Subhadiptech <subhadipdhn@gmail.com>
2023-11-06 17:45:38 +0530

    font and color added

---
+++ app/src/main/java/com/ersubhadip/devmastermind_gamifyyourdevelopmentjourney/ui/theme/Font.kt
@@ -0,0 +1,12 @@
+package com.ersubhadip.devmastermind_gamifyyourdevelopmentjourney.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.ersubhadip.devmastermind_gamifyyourdevelopmentjourney.R
+
+val LexendDecaLight = FontFamily(Font(R.font.lexend_deca_light))
+val LexendDecaRegular = FontFamily(Font(R.font.lexend_deca_regular))
+val LexendDecaSemiBold = FontFamily(Font(R.font.lexend_deca_semi_bold))
+val LexendDecaBold = FontFamily(Font(R.font.lexend_deca_bold))
+val LexendDecaExtraBold = FontFamily(Font(R.font.lexend_deca_extra_bold))
+val AlegreyaSansRegular = FontFamily(Font(R.font.alegreya_sans_regular))
