38b06e85263fbb9fd8202b1f652c4dd3a167948c
Subhadiptech <subhadipdhn@gmail.com>
2023-11-06 17:43:05 +0530

    initialized project

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Dev Mastermind- Gamify your development journey"
+include(":app")
