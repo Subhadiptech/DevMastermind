9e0a6f735aa592039260a6ee5a6948334b3d4cdf
Subhadiptech <subhadipdhn@gmail.com>
2023-11-07 14:13:59 +0530

    hilt fix

--- app/src/main/java/com/ersubhadip/devmastermind_gamifyyourdevelopmentjourney/presentation/details/CourseDetailsViewModel.kt
+++ app/src/main/java/com/ersubhadip/devmastermind_gamifyyourdevelopmentjourney/presentation/details/CourseDetailsViewModel.kt
@@ -1,10 +1,12 @@
 package com.ersubhadip.devmastermind_gamifyyourdevelopmentjourney.presentation.details
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class CourseDetailsViewModel @Inject constructor(
 
 ) : ViewModel() {
-  
+
 }
